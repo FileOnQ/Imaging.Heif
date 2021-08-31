b36bd756797c0e820812350aebac659871ed4533
Andrew Hoefling <andrewh@fileonq.com>
2021-08-30 23:47:21 -0400

    Updated build to use debug version of libjpeg-turbo as release doesn't write to disk correctly

--- build/libjpeg-turbo.bat
+++ build/libjpeg-turbo.bat
@@ -4,4 +4,4 @@ set arch=%1
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %arch%
 cd ../third-party/libjpeg-turbo
 cmake .
-msbuild libjpeg-turbo.sln /p:Configuration=Release
+msbuild libjpeg-turbo.sln /p:Configuration=Debug
