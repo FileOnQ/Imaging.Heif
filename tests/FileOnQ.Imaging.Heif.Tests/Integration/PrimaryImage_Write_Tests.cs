02e8d986e44552765bd8dbf3b8fde1cc547f32d1
Andrew Hoefling <andrewh@fileonq.com>
2021-09-03 09:58:22 -0400

    Added write to disk tests

---
+++ tests/FileOnQ.Imaging.Heif.Tests/Integration/PrimaryImage_Write_Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using FileOnQ.Imaging.Heif.Tests.Utilities;
+using NUnit.Framework;
+
+namespace FileOnQ.Imaging.Heif.Tests.Integration
+{
+	[TestFixture(TestData.Image1)]
+	[TestFixture(TestData.Image2)]
+	[TestFixture(TestData.Image3)]
+	[TestFixture(TestData.Image4)]
+	[TestFixture(TestData.Image5)]
+	[Category(Constants.Category.Integration)]
+    public class PrimaryImage_Write_Tests
+	{
+		readonly string input;
+		readonly string output;
+		readonly string hash;
+
+		public PrimaryImage_Write_Tests(string path)
+		{
+			hash = TestData.Integration.PrimaryImageWrite.HashCodes[path];
+
+			var assemblyDirectory = Path.GetDirectoryName(typeof(NoThumbnailTests).Assembly.Location) ?? string.Empty;
+			input = Path.Combine(assemblyDirectory, path);
+
+			var filename = Path.GetFileNameWithoutExtension(input);
+			var directory = Path.GetDirectoryName(input) ?? string.Empty;
+			output = Path.Combine(directory, $"{filename}.jpeg");
+		}
+
+		[OneTimeSetUp]
+		public void Execute()
+		{
+			using (var image = new HeifImage(input))
+			using (var primary = image.PrimaryImage())
+			{
+				primary.Write(output);
+			}
+		}
+
+		[OneTimeTearDown]
+		public void TearDown()
+		{
+			if (File.Exists(output))
+				File.Delete(output);
+		}
+
+		[Test]
+		public void PrimaryImage_Write_FileExists_Test() =>
+			Assert.IsTrue(File.Exists(output));
+
+		[Test]
+		public void PrimaryImage_Write_Match_Test()
+		{
+			var buffer = File.ReadAllBytes(output);
+
+			Assert.IsTrue(buffer.Length > 0);
+			AssertUtilities.IsHashEqual(hash, buffer);
+		}
+    }
+}
---
+++ tests/FileOnQ.Imaging.Heif.Tests/TestData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace FileOnQ.Imaging.Heif.Tests
+{
+	public static class TestData
+    {
+		public const string Image1 = "Images\\20210224_023830752_iOS.heic";
+		public const string Image2 = "Images\\20210224_024608242_iOS.heic";
+		public const string Image3 = "Images\\20210224_024634111_iOS.heic";
+		public const string Image4 = "Images\\20210224_032806886_iOS.heic";
+		public const string Image5 = "Images\\20210821_095129.heic";
+
+		public static class Integration
+		{
+			public static class PrimaryImageWrite
+			{
+				public static IDictionary<string, string> HashCodes => BuildDictionary();
+
+				static IDictionary<string, string> BuildDictionary() =>
+					new Dictionary<string, string>
+					{
+						{ Image1, "FFEA4484A3DE2DBE3D85321EF5ABDFF2C5D4367F6D77F2899CB0D62B270A9388" },
+						{ Image2, "AA9A7640A97AA5824ECBD84C3CDCB23117A7E01BC7A313C6390B0702559579A7" },
+						{ Image3, "D544E3BA40215AE671DE0D4474489F596AA6AFB27F9774E6C3B322242B17634F" },
+						{ Image4, "51BF8E1F356A618E3D86E13560B557B91CB6C6269F0C6298BEBDC447675A4250" },
+						{ Image5, "0B223CCEE9712EAD50F51E0AE96DA5276DE98D0ED48B60D958B354B228B88B01" }
+					};
+			}
+		}
+	}
+}
