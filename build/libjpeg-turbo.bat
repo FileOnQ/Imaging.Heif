51f1f7799843241cac53a3021148ab182d7579a2
Andrew Hoefling <andrewh@fileonq.com>
2022-01-05 13:43:19 -0500

    Compile libjpeg-turbo under current build Configuration

--- build/libjpeg-turbo.bat
+++ build/libjpeg-turbo.bat
@@ -1,7 +1,8 @@
 @echo off
 title Building libjpeg-turbo
 set arch=%1
+set config=%2
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %arch%
 cd ../third-party/libjpeg-turbo-%arch%
-cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Debug .
+cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=%config% .
 nmake
