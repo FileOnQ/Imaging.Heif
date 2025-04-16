810969a6d1102a151d811b5a7c284d25585a74f5
Mitchel Sellers <msellers@iowacomputergurus.com>
2025-04-16 09:43:46 -0500

    Attempt to force relese build only

--- build/libheif.bat
+++ build/libheif.bat
@@ -55,7 +55,7 @@ mkdir build
 cd build
 
 cmake -G"NMake Makefiles"^
- -DCMAKE_BUILD_TYPE=%config%^
+ -preset:release^
  -DDAV1D_INCLUDE_DIR:PATH=%thirdPartyPath%\dav1d-%arch%\build\include^
  -DDAV1D_LIBRARY:FILEPATH=%thirdPartyPath%\dav1d-%arch%\build\src\dav1d.lib^
  -DJPEG_INCLUDE_DIR:PATH=%thirdPartyPath%\libjpeg-turbo-%arch%^
